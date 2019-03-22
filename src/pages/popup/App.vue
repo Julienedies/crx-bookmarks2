b0c36ac3166eeb189011a53c5b4202dd295e636e
julienedies <julienedies@gmail.com>
2019-03-22 20:59:19 +0800

    optimize

--- src/pages/popup/App.vue
+++ src/pages/popup/App.vue
@@ -1,26 +1,29 @@
 <template>
     <div class="box">
 
-        <div>
-
-            <div style="text-align:right; padding:0.5rem;">
-                <button class="button" @click="isSetBookmark=1" v-if="bookmark.id">修改书签</button>
-                <button class="button" @click="isSetBookmark=1" v-else>添加书签</button>
+        <div class="level">
+            <div class="level-left buttons has-addons">
                 <button class="button" :class="{'is-info':c==='shortcut'}" @click="c='shortcut'">快捷</button>
                 <button class="button" :class="{'is-info':c==='recent'}" @click="c='recent'">最近</button>
                 <button class="button" :class="{'is-info':c==='hot'}" @click="c='hot'">常用</button>
+            </div>
+
+            <div class="level-right buttons has-addons">
+                <button class="button" @click="isSetBookmark=1" v-if="bookmark.id">修改书签</button>
+                <button class="button" @click="isSetBookmark=1" v-else>添加书签</button>
+                <button class="button" @click="addShortcut" v-show="bookmark.id">添加快捷方式</button>
                 <button class="button" @click="open">打开书签管理器</button>
             </div>
-
-            <div class="">
-                <component :is="c"></component>
-            </div>
-
-            <div class="layer" :class="{show:isSetBookmark}">
-                <setBookmark :bookmark="bookmark" @close="isSetBookmark=false"></setBookmark>
-            </div>
-
         </div>
+
+        <div>
+            <component :is="c"></component>
+        </div>
+
+        <div class="layer" :class="{show:isSetBookmark}">
+            <setBookmark :bookmark="bookmark" @close="isSetBookmark=false"></setBookmark>
+        </div>
+
     </div>
 </template>
 
@@ -34,6 +37,7 @@
     import setBookmark from './setBookmark'
 
     const visitDb = getDb('visit')
+    const shortcutDb = getDb('shortcut')
 
     export default {
         name: 'App',
@@ -67,8 +71,14 @@
             },
             async getBookmarkByTab () {
                 let tab = await tabs.getSelected()
+                console.log('#tab is', tab)
                 let bookmarkArray = await bookmarks.search(tab.url)
-                if (bookmarkArray && bookmarkArray.length === 1) {
+                console.log('#search', bookmarkArray)
+                bookmarkArray = bookmarkArray || []
+                bookmarkArray = bookmarkArray.filter( (item) => {
+                    return item.url === tab.url
+                })
+                if (bookmarkArray && bookmarkArray.length) {
                     this.bookmark = bookmarkArray[0]
                     bookmarks.get(this.bookmark.parentId).then((data) => {
                         this.$set(this.bookmark, 'folderName', data[0].title)
@@ -76,6 +86,11 @@
                 } else {
                     this.bookmark = {title: tab.title, url: tab.url}
                 }
+            },
+            addShortcut () {
+                shortcutDb.set(this.bookmark).then(data => {
+                    this.$msg('添加完成!')
+                })
             },
         }
     }
@@ -95,7 +110,7 @@
         opacity: 0;
 
         &.show {
-            top:0;
+            top: 0;
             opacity: 1;
         }
     }
